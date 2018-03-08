7a654915e186b734b79470627175f878f25c9cd3
Xavi Querol <javier.querol.sge@mango.com>
2018-03-08 11:23:50 +0100

    Testing Logback.

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Different tests with Spring Boot.
 
 ### v0.0.1-SNAPSHOT
 * Build JAR or WAR based on pom.xml profiles.
+
+### v0.0.2-SNAPSHOT
+* Change of profile name from 'other' to 'nodev'.
+* Log configuration using Logback.
--- src/main/java/cat/acme/springbootexample/controllers/HelloWorldController.java
+++ src/main/java/cat/acme/springbootexample/controllers/HelloWorldController.java
@@ -3,11 +3,20 @@ package cat.acme.springbootexample.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class HelloWorldController {
 
 	@RequestMapping("/hello")
 	public String helloWorld() {
+		if (HelloWorldController.log.isDebugEnabled()) {
+			HelloWorldController.log.debug("[helloWorld] ::: Debug message.");
+		}
+		if (HelloWorldController.log.isInfoEnabled()) {
+			HelloWorldController.log.info("[helloWorld] ::: Info message.");
+		}
 		return "Hello world!";
 	}
 
